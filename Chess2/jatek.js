cb9a4510d534c623f290e229876a279ef6d6ef70
KD229 <112970437+KD229@users.noreply.github.com>
2023-11-16 13:06:30 +0100

    Update jatek.js

--- Chess2/jatek.js
+++ Chess2/jatek.js
@@ -51,7 +51,7 @@ class character {
                 this.imageIndex = 0
                 this.draw = function() {
                     drawRect(260+this.x*120 - ((this.x-this.startX)*120-(this.y-this.startY)*40)*(this.stepping/this.stepTime)-camX-this.y*40,
-                    25+this.y*50+-((this.y-this.startY)*50)*(this.stepping/this.stepTime)-camY,65,
+                    25+this.y*50-((this.y-this.startY)*50)*(this.stepping/this.stepTime)-camY,65,
                     40,"black",0.5)
                     // x: Offset of lowest row + position on x tyles - Movement, both in x and y axis - camera offset - adjusting for board diagonalness
                     // y: Offset for the height of the image + position on y tyles + Jump curve - Movement in y axis - camera offset
@@ -61,11 +61,11 @@ class character {
 
                 this.stepping = 0
                 this.update = function() {
-                    //if (this.stepping == 0 && stepDir != null) {this.step(stepDir)}
-                    if (this.stepping == 0) {this.step(pathTo(xgoal,ygoal,this.x,this.y))}
-                    if (this.stepping == Math.round(this.steptime / 2)) {console.log("ZOINKS")}
-                    if (this.stepping == 1) {this.stepFinish()}
                     if (this.stepping != 0) {this.stepping -= 1}
+                    if (this.stepping == 0 && stepDir != null) {this.step(stepDir)}
+                    if (this.stepping == Math.round(this.steptime / 2)) {console.log("ZOINKS")}
+                    if (this.stepping == 0) {this.stepFinish()}
+                    //if (this.stepping == 0) {this.step(pathTo(xgoal,ygoal,this.x,this.y))}
                 }
                 this.step = function(direction) {
                     if (direction == undefined) {return}
@@ -101,20 +101,71 @@ class character {
 
                 this.imageIndex = 1
                 this.draw = function() {
-                    drawRect(260+this.x*120 - ((this.x-this.startX)*120-(this.y-this.startY)*40)*(this.stepping/this.stepTime)-camX-this.y*40,
-                    25+this.y*50+-((this.y-this.startY)*50)*(this.stepping/this.stepTime)-camY,65,
-                    40,"black",0.5)
-                    drawImage(images[this.imageIndex], 260+this.x*120 - ((this.x-this.startX)*120-(this.y-this.startY)*40)*(this.stepping/this.stepTime)-camX-this.y*40,
-                    95+this.y*50+(60*Math.sin((Math.PI/2)*(this.stepping/this.stepTime)))-((this.y-this.startY)*50)*(this.stepping/this.stepTime)-camY,1)
+                    switch (this.stepphase) {
+                        case 3: 
+                            drawRect(260+this.x*120-camX-this.y*40,25+this.y*50-camY,65,40,"black",0.5)
+                            drawImage(images[this.imageIndex], 260+this.x*120-camX-this.y*40,
+                            95+this.y*50+(60*Math.sin(Math.PI/2))*(1-this.stepping/this.stepTime)-camY,1)
+                        break
+                        case 2:
+                            drawRect(260+this.x*120-camX-this.y*40,25+this.y*50-camY,65,40,"black",0.5)
+                            drawImage(images[this.imageIndex], 260+this.x*120-camX-this.y*40,
+                            95+this.y*50+(60*Math.sin(Math.PI/2))-camY,1)
+                        break
+                        
+                        case 1: 
+                        drawRect(260+this.x*120 - ((this.x-this.startX)*120-(this.y-this.startY)*40)*(this.stepping/this.stepTime)-camX-this.y*40,
+                        25+this.y*50+-((this.y-this.startY)*50)*(this.stepping/this.stepTime)-camY,65,
+                        40,"black",0.5)
+                        drawImage(images[this.imageIndex], 260+this.x*120 - ((this.x-this.startX)*120-(this.y-this.startY)*40)*(this.stepping/this.stepTime)-camX-this.y*40,
+                        95+this.y*50+(60*Math.sin((Math.PI/2)*(this.stepping/this.stepTime)))-((this.y-this.startY)*50)*(this.stepping/this.stepTime)-camY,1)
+                        break
+                        case 4:
+                        case 0:
+                            drawRect(260+this.x*120-camX-this.y*40,25+this.y*50-camY,65,40,"black",0.5)
+                            drawImage(images[this.imageIndex], 260+this.x*120-camX-this.y*40,
+                            95+this.y*50-camY,1)
+                            console.log("sss")
+                        break
+                    }
+                    
                 }
                 this.stepping = 0
                 this.stepphase = 0
+                //STEP PHASES:  0: Standing Still   1: Jumping  2: Standing still in air    3:Smashing down 4: Cooling down
                 this.update = function() {
-                    if ( this.stepphase == 0 && this.stepping == 0) {this.step(pathTo(player.y,player.x,this.x,this.y))}
-                    if (this.stepphase == 1 && this.stepping == Math.round(this.steptime / 2)) {console.log("ZOINKS")}
-                    if (this.stepping == 1) {this.stepFinish()}
                     if (this.stepping != 0) {this.stepping -= 1}
+                    else if (this.stepping == 0 && this.stepphase == 3) {this.stepFinish()}
+                    if (this.stepphase == 0 && this.stepping == 0) {
+                        this.step(pathTo(player.y,player.x,this.x,this.y))
+                    }
+                    else if (this.stepphase == 1 && this.stepping == 0) {this.stepphase = 2; this.stepping = this.airtime}
+                    else if (this.stepphase == 2 && this.stepping == 0) {this.stepphase = 3; this.stepping = this.falltime}
+                    else if (this.stepphase == 3 && this.stepping == 0) {this.stepphase = 4; this.stepping = this.cooldown}
+                    else if (this.stepphase == 1 && this.stepping == Math.round(this.steptime / 2)) {console.log("ZOINKS")}
                 }
+                this.step = function(direction) {
+                    if (direction == undefined) {return}
+                    this.stepping = this.stepTime
+                    tilelist[this.x][this.y].characters = tilelist[this.x][this.y].characters.filter((id) => id != this.id)
+                    characterRenderList[this.y] = characterRenderList[this.y].filter((id) => id != this.id)
+                    this.startX = this.x
+                    this.startY = this.y
+                    switch(direction) {
+                        case 0: this.x += 1; break
+                        case 1: this.x -= 1; break
+                        case 2: this.y += 1; break
+                        case 3: this.y -= 1; break
+                    }
+                    tilelist[this.x][this.y].characters.push(this.id)
+                }
+                this.stepFinish = function() {
+                    if (this.startY < this.y) {
+                        characterRenderList[this.startY] = characterRenderList[this.startY].filter((id) => id != this.id)
+                        characterRenderList[this.y].push(this.id)
+                    }
+                }
+            break       
         }
         this.id = characterIndex
         tilelist[this.x][this.y].characters.push(this.id)
@@ -156,6 +207,8 @@ function drawGame() {
 //  GAME!!
 function start() {
     player = new character(0, 2, 3)
+    sus = new character(1, 4, 4)
+    console.log(sus)
     context.imageSmoothingEnabled = false
     gameLoop = setInterval( function() {
         characterList.forEach(character => {
