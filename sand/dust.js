4e900a4fb032c5300123a88eef12f687afad4c0e
KD229 <112970437+KD229@users.noreply.github.com>
2025-03-19 13:56:33 +0100

    Update dust.js

--- sand/dust.js
+++ sand/dust.js
@@ -1,20 +1,27 @@
 let Mdown = false
 x = 0
 y = 0
+cursorSize = 1
 waitTime = 5
-width = 500
-height = 500
+let realWidth = 0
+let realHeight = 0
+width = 250
+height = 250
 dustcoords = Array(width).fill().map(() => Array(height).fill().map(() => 0))
 console.log(dustcoords)
 
 function drawRect(x,y,width,height,color,alpha = 1) {
     context.globalAlpha = alpha
-    context.setTransform(1, 0, 0, 1, x, -y+500)
+    context.setTransform(1, 0, 0, 1, x, -y+realHeight)
     context.fillStyle = color
     context.fillRect(-width/2,-height/2,width,height)
     context.resetTransform()
 }
 window.onload = function() {
+    realWidth = document.getElementById("canvas").width
+    realHeight = document.getElementById("canvas").height
+    widthRatio = Math.round(realWidth/width)
+    heightRatio = Math.round(realHeight/height)
     context = document.getElementById("canvas").getContext("2d")
     start()
 }
@@ -25,8 +32,8 @@ function down(event) {
     Mdown = true
 }
 function move(event) {
-    x = Math.round(event.offsetX)
-    y = Math.round(-event.offsetY+height)
+    x = Math.round(event.offsetX*width/realWidth)
+    y = Math.round((-event.offsetY+realHeight)*height/realHeight)
 }
 
 class particle {
@@ -35,10 +42,15 @@ class particle {
         this.color = [r, g, b]
     }
     draw(x,y) {
-        //console.log("rgb("+this.color[0]+", "+this.color[1]+", "+this.color[2])
-        drawRect(x,y,1,1,"rgb("+this.color[0]+", "+this.color[1]+", "+this.color[2])
+        drawRect(x*widthRatio,y*heightRatio,widthRatio,heightRatio,"rgb("+this.color[0]+", "+this.color[1]+", "+this.color[2])
     }
-    fall(x,y) {
+    
+}
+class falling extends particle {
+    constructor() {
+        super(1, 211+Math.round(Math.random()*20)-10, 169+Math.round(Math.random()*20)-10, 108+Math.round(Math.random()*20)-10)
+    }
+    update(x,y) {
         if (y > 0) {
             if (y > 0 && dustcoords[x][y-1] == 0) {
                 dustcoords[x][y] = 0
@@ -50,7 +62,7 @@ class particle {
                     dustcoords[x-1][y-1] = this
                 }
             else {
-                if (x < width && dustcoords[x+1][y-1] == 0 && dustcoords[x+1][y] == 0) {
+                if (x < width-1 && dustcoords[x+1][y-1] == 0 && dustcoords[x+1][y] == 0) {
                     dustcoords[x][y] = 0
                     dustcoords[x+1][y-1] = this
                 }
@@ -64,18 +76,19 @@ function start() {
     context.imageSmoothingEnabled = false
     gameLoop = setInterval( function() {
         if (Mdown) {
-            for (let i = x-5; i < x+5 && i < width && i > 0; i++) {
-                for (let ii = y-5; ii < y+5 && ii < height && ii > 0; ii++) {
-                    dustcoords[i][ii] = new particle(0,Math.round(Math.random()*255),Math.round(Math.random()*255),Math.round(Math.random()*255))
+            for (let ii = y-Math.floor(cursorSize/2); ii < y+Math.ceil(cursorSize/2) && ii < width && ii > 0; ii++) {
+                for (let i = x-Math.floor(cursorSize/2); i < x+Math.ceil(cursorSize/2) && i < height && i > 0; i++) {
+                    //dustcoords[i][ii] = new particle(0,Math.round(Math.random()*255),Math.round(Math.random()*255),Math.round(Math.random()*255))
+                    dustcoords[i][ii] = new falling()
                 }
             }
         }
-        drawRect(250,250,height+1, width+1,"grey")
-        for (let ix = 0; ix < width; ix++) {
-            for (let iy = 0; iy < height; iy++) {
+        drawRect(Math.floor(realWidth/2),Math.floor(realHeight/2),realWidth+1, realHeight+1,"grey")
+        for (let iy = 0; iy < width; iy++) {
+            for (let ix = 0; ix < height; ix++) {
                 if (dustcoords[ix][iy] != 0) {
                     dustcoords[ix][iy].draw(ix,iy)
-                    dustcoords[ix][iy].fall(ix,iy)
+                    dustcoords[ix][iy].update(ix,iy)
             }
                 
             }
